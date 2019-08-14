b417731eaeb8fc127eaf82138217716d1668b0f1
Prince Alvin Yusuf <princealvinyusuf@github.com>
2019-08-14 18:33:57 +0700

    Initial commit

---
+++ app/src/main/java/com/kominfo/go/aplikasisqlite/activity/AddEditActivity.java
@@ -0,0 +1,122 @@
+package com.kominfo.go.aplikasisqlite.activity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.kominfo.go.aplikasisqlite.R;
+import com.kominfo.go.aplikasisqlite.helper.DbHelper;
+
+public class AddEditActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText txt_id, txt_name, txt_address;
+    Button btn_submit, btn_cancel;
+    DbHelper SQLite = new DbHelper(this);
+    String id, name, address;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_edit);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        txt_id = findViewById(R.id.txt_id);
+        txt_name = findViewById(R.id.txt_name);
+        txt_address = findViewById(R.id.txt_address);
+        btn_submit = findViewById(R.id.btn_submit);
+        btn_submit.setOnClickListener(this);
+        btn_cancel = findViewById(R.id.btn_cancel);
+        btn_cancel.setOnClickListener(this);
+
+        id = getIntent().getStringExtra(MainActivity.TAG_ID);
+        name = getIntent().getStringExtra(MainActivity.TAG_NAME);
+        address = getIntent().getStringExtra(MainActivity.TAG_ADDRESS);
+
+        if (id == null || id == "") {
+            setTitle("Add Data");
+        } else {
+            setTitle("Edit Data");
+            txt_id.setText(id);
+            txt_name.setText(name);
+            txt_address.setText(address);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.btn_submit:
+                try {
+                    if (txt_id.getText().toString().equals("")) {
+                        save();
+                    } else {
+                        edit();
+                    }
+                } catch (Exception e) {
+                    Log.e("submit", e.toString());
+                }
+                break;
+
+            case R.id.btn_cancel:
+                blank();
+                finish();
+                break;
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                blank();
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void blank() {
+        txt_name.requestFocus();
+        txt_id.setText(null);
+        txt_name.setText(null);
+        txt_address.setText(null);
+    }
+
+    private void save() {
+        if (String.valueOf(txt_name.getText()).equals(null) || String.valueOf(txt_name.getText()).equals("") ||
+                String.valueOf(txt_address.getText()).equals(null) || String.valueOf(txt_address.getText()).equals("")) {
+            Toast.makeText(getApplicationContext(), "Please Input Name or Address", Toast.LENGTH_SHORT).show();
+        } else {
+            SQLite.insert(txt_name.getText().toString().trim(), txt_address.getText().toString().trim());
+            finish();
+        }
+    }
+
+    private void edit() {
+        if (String.valueOf(txt_name.getText()).equals(null) || String.valueOf(txt_name.getText()).equals("") ||
+                String.valueOf(txt_address.getText()).equals(null) || String.valueOf(txt_address.getText()).equals("")) {
+            Toast.makeText(getApplicationContext(), "Please Input Name or Address", Toast.LENGTH_SHORT).show();
+        } else {
+            SQLite.update(Integer.parseInt(txt_id.getText().toString().trim()), txt_name.getText().toString().trim(),
+                    txt_address.getText().toString().trim());
+            blank();
+            finish();
+        }
+    }
+
+
+}
